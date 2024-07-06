5b7bc8f4e74c21952ed26c5b102af8138da1511c
Iryna P <irpolg@ukr.net>
2024-07-06 11:40:56 +0300

    1hw-2generics-test

---
+++ src/generics/1.ts
@@ -0,0 +1,24 @@
+// Типізуйте асинхронну функцію fetchData, я
+// ка приймає URL ресурсу та повертає об'єкт з даними. 
+// Використовуйте Generics для типізації повернутих даних.
+// import axios from 'axios';
+// async function fetchData(url) {
+//   try {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(`Error fetching from ${url}: ${error}`);
+//   }
+// }
+
+import axios from 'axios';
+
+async function fetchData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
+console.log('hello');
