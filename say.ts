a72d1680051f69bda0650b36d7d70df830e99e49
Iryna P <irpolg@ukr.net>
2024-07-06 08:48:06 +0300

    1hw-ts-1task

--- say.ts
+++
@@ -1,4 +0,0 @@
-function sayHello(name: string): void {
-    console.log(`Hello, ${name}!`);
-}
-let result = sayHello("John");
---
+++ src/basic/1.ts
@@ -0,0 +1,14 @@
+//Є наступний JavaScript код. Перетворіть цей код 
+//на TypeScript, вказавши відповідні типи для всіх змінних.
+// const age = 50;
+// const username = 'Max';
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => { return 100 + a };
+
+const age: number=50;
+const username: string="Max";
+const toggle: boolean = true;
+const empty: null = null;
+const callback = (a: number): number => { return 100 +a };
+
