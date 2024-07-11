65e4a45b77e3249f09e323e1ed8fff43fe44a667
Iryna P <irpolg@ukr.net>
2024-07-11 07:05:09 +0300

    1hw-3all-yes

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,6 +1,6 @@
 // Завдання 3
-// У вас є функція merge, яка поєднує два об'єкти. 
-// Використовуйте generics, щоб вказати, що ці об'єкти 
+// У вас є функція merge, яка поєднує два об'єкти.
+// Використовуйте generics, щоб вказати, що ці об'єкти
 // можуть бути будь-якого типу.
 // function merge (objA, objB) {
 //     return Object.assign(objA, objB);
@@ -11,10 +11,14 @@
 //   }
 
 //обмеження, що T та U мають бути об'єктами
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+// function merge<T extends object, U extends object>(objA: T, objB: U) {
+//   return Object.assign(objA, objB);
+// }
+
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
-const merged = merge({ name: 'Alisa' }, { age: 28 });
-console.log("merged>> ", merged)
+const merged = merge({ name: "Alisa" }, { age: 28 });
+console.log("merged>> ", merged);
 export {};
